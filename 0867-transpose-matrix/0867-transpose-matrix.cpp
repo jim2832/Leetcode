dad213c8c09e0b8ad38480c5cf2d8fd75e9f9c89
Jim Lee <78035176+jim2832@users.noreply.github.com>
2023-06-12 13:16:12 +0800

    Time: 16 ms (35.99%), Space: 10.5 MB (67.45%) - LeetHub

---
+++ 0867-transpose-matrix/0867-transpose-matrix.cpp
@@ -0,0 +1,15 @@
+class Solution{
+    public:
+        vector< vector<int> > transpose(vector<vector<int>>& matrix){
+            int row = matrix.size();
+            int column = matrix[0].size();
+            vector<vector<int>> result(column, vector<int>(row)); //存結果
+
+            for(int i=0; i<row; i++){
+                for(int j=0; j<column; j++){
+                    result[j][i] = matrix[i][j];
+                }
+            }
+            return result;
+        }          
+};

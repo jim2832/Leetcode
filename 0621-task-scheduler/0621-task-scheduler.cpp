9f372e51edd119e96258d9c1bae54911d3151e60
Jim Lee <78035176+jim2832@users.noreply.github.com>
2023-06-12 13:15:18 +0800

    Time: 137 ms (24.77%), Space: 43.9 MB (24.16%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    public:
+        int leastInterval(vector<char>& tasks, int n) {
+            //紀錄每種 tasks 有幾個
+            unordered_map<char, int> counts;
+            for(char t:tasks) counts[t]++;
+
+            //將 tasks 的數量 push 進 priority queue
+            priority_queue<int> pq;
+            for(auto c:counts) pq.push(c.second);
+
+            int result = 0;
+            while(!pq.empty()){
+                int time = 0;
+                vector<int> temp;
+                for(int i=0; i<n+1; ++i){
+                    if(!pq.empty()){
+                        temp.push_back(pq.top()-1);
+                        pq.pop();
+                        time++;
+                    }
+                }
+                for(auto t:temp)
+                    if(t) pq.push(t);
+                result += pq.empty() ? time : n+1;
+            }
+            return result;
+        }
+};

7f955607125878f9c944ac90da6d60fefd767243
Jim Lee <78035176+jim2832@users.noreply.github.com>
2023-07-23 01:49:10 +0800

    Time: 146 ms (94.98%), Space: 66.6 MB (93.11%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution{
+    public: 
+        vector<int> sortArray(vector<int>& nums){
+            sort(nums.begin(), nums.end(), less<int>());
+
+            return nums;
+        }
+};

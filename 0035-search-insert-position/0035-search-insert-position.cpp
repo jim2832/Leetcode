95636c23e032903868ee3b044e95c8a413e1d034
Jim Lee <78035176+jim2832@users.noreply.github.com>
2023-07-26 14:53:14 +0800

    Time: 7 ms (36.31%), Space: 9.7 MB (48.15%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,9 +1,32 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        return Binary_search(nums, 0, nums.size()-1, target);
+        //recursion version
+        //return Binary_search(nums, 0, nums.size()-1, target);
+        
+        //iteration version
+        int lower = 0;
+        int upper = nums.size()-1;
+        int position;
+        while(upper >= lower){
+            int mid = (lower + upper) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] > target){
+                upper = mid-1;
+                position = mid;
+            }
+            else if(nums[mid] < target){
+                lower = mid+1;
+                position = mid+1;
+            }
+        }
+        return position;
     }
-
+    
+    //recursion version
+    /*
     int Binary_search(vector<int> &nums, int lower, int upper, int target){
         if(upper < lower){
             return lower;
@@ -20,4 +43,5 @@ public:
             return Binary_search(nums, mid+1, upper, target);
         }
     }
+    */
 };

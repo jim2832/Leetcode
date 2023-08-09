0685679e4432bfe6c83fc7cab4ee6696f2982425
Jim Lee <78035176+jim2832@users.noreply.github.com>
2023-08-09 13:39:29 +0800

    Time: 0 ms (100.00%), Space: 13.6 MB (70.47%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -11,6 +11,7 @@
 // = min(100, 2) + 1
 // = 3
 
+/*
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
@@ -31,5 +32,29 @@ public:
         }
 
         return step[len];
+    }
+};
+*/
+
+// 更好的解法
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int len = cost.size();
+        int step_1 = cost[0];
+        int step_2 = cost[1];
+        int step_3;
+
+        // 處理邊界條件
+        cost.push_back(0);
+
+        for(int i=2; i<=len; i++){
+            step_3 = min(step_1, step_2) + cost[i];
+            //重複利用變數
+            step_1 = step_2;
+            step_2 = step_3;
+        }
+
+        return step_3;
     }
 };

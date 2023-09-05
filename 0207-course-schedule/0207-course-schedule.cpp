79a2273f69dab95908e27dd08166a42d145b787f
Jim Lee <78035176+jim2832@users.noreply.github.com>
2023-09-05 20:35:49 +0800

    Time: 19 ms (80.20%), Space: 13.9 MB (42.05%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -9,6 +9,7 @@
 
 */
 
+/*
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
@@ -50,5 +51,56 @@ public:
 
         //若最後 in degree 為 0 的節點數等於總節點數，則代表有一組 topological_sort
         return (visited==numCourses);
+    }
+};
+*/
+
+//DFS解法
+class Solution {
+    vector<int> color;
+    vector<vector<int>> edges; //紀錄相鄰邊
+
+    bool check_cycle(int vertex){
+        color[vertex] = 1; //走訪後設定成灰色
+
+        for(int i=0; i<edges[vertex].size(); i++){
+            if(color[edges[vertex][i]] == 0){ //如果是白點，則可以繼續走訪
+                bool cycled = check_cycle(edges[vertex][i]); //繼續往下走訪
+                if(cycled){ //有環
+                    return true;
+                }
+            }
+            //有環(有走訪到黑點)
+            else if(color[edges[vertex][i]] == 1){
+                return true;
+            }
+        }
+        
+        color[vertex] = 2; //走訪後設定成黑色
+        return false;
+    }
+
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        color.resize(numCourses);
+        edges.resize(numCourses);
+
+        for(auto edge:prerequisites){
+            // edge[1] -> edge[0]
+            edges[edge[1]].push_back(edge[0]);
+        }
+
+        //DFS
+        for(int i=0; i<numCourses; i++){
+            if(color[i] == 0){ //找白點
+                //檢查是否有環，如果有，則無法符合題目條件
+                bool cycled = check_cycle(i);
+                if(cycled){
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 };

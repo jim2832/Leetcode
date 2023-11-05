a37fc28c46f58d937d04d20560e4d7026e425ee8
Jim Lee <78035176+jim2832@users.noreply.github.com>
2023-11-05 14:32:02 +0800

    Time: 0 ms (100.00%), Space: 6.6 MB (41.77%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     bool checkString(string s) {
-        bool encounter = false; // if we have encountered "b"
-        for(int i=0; i<s.size(); i++){
-            if(s[i] == 'a'){
-                if(encounter){
-                    return false;
-                }
-                else{
-                    continue;
-                }
-            }
-            else{ //s[i] == b
-                encounter = true;
-            }
-        }
+        // appraoch 1
+        // bool encounter = false; // if we have encountered "b"
+        // for(int i=0; i<s.size(); i++){
+        //     if(s[i] == 'a'){
+        //         if(encounter){
+        //             return false;
+        //         }
+        //         else{
+        //             continue;
+        //         }
+        //     }
+        //     else{ //s[i] == b
+        //         encounter = true;
+        //     }
+        // }
 
-        return true;
+        // return true;
+
+        // approach 2
+        string temp = s;
+        sort(s.begin(), s.end());
+        return s == temp;
     }
 };

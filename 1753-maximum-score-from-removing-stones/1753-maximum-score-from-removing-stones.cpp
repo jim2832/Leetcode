4f9825c515cd37d4bfc890800792f11177de2f12
Jim Lee <78035176+jim2832@users.noreply.github.com>
2023-12-03 22:44:56 +0800

    Time: 57 ms (63.27%), Space: 6.4 MB (54.69%) - LeetHub

--- 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
+++ 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
@@ -2,13 +2,10 @@ class Solution {
 public:
     int maximumScore(int a, int b, int c) {
         int score = 0;
-        vector<int> stones;
-        stones.insert(stones.end(), {a, b, c});
+        vector<int> stones = {a, b, c};
         sort(stones.begin(), stones.end());
         
-        while(stones[1] && stones[2]){
-            stones[1]--;
-            stones[2]--;
+        while(stones[1]-- && stones[2]--){
             score++;
             sort(stones.begin(), stones.end());
         }

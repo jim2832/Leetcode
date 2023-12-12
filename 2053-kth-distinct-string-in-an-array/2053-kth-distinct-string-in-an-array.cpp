412a483e72262ab2312eec29160f5f1212afa18d
Jim Lee <78035176+jim2832@users.noreply.github.com>
2023-12-13 00:00:59 +0800

    Time: 13 ms (79.30%), Space: 15.2 MB (10.25%) - LeetHub

---
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+        unordered_map<string, int> hash;
+        priority_queue<pair<int, string>> pq;
+        
+        for(auto &str:arr){
+            hash[str]++;
+        }
+        
+        for(auto &num:arr){
+            if(hash[num] < 2){
+                k--;
+            }
+            if(k == 0){
+                return num;
+            }
+        }
+
+        return "";
+    }
+};

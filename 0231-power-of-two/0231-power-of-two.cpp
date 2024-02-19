6620ca74e7027b76d54325a572da048a8d807c8e
Jim Lee <78035176+jim2832@users.noreply.github.com>
2024-02-19 19:53:48 +0800

    Time: 0 ms (100.00%), Space: 7.1 MB (39.77%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,8 +1,24 @@
+// bit manipulation solution
+// 若一個數是2的次方次數，他的二進位只會有一個1
+// 8 -> 00001000
+// 7 -> 00000111
+// 若 n 和 n-1 做 bitwise and 為 0，則代表他是2的次方數
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n == 0)
-            return false;
+        if(n <= 0) return false;
+        return (n & n-1) == 0;
+    }
+};
+
+/*
+// division solution
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        // exception
+        if(n == 0) return false;
+
         while(n % 2 == 0){
             n /= 2;
         }
@@ -10,3 +26,4 @@ public:
         return n == 1;
     }
 };
+*/

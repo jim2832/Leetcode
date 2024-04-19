a8e30be4c07b93efe444375af55023ac0b44b4e2
Jim Lee <78035176+jim2832@users.noreply.github.com>
2024-04-19 16:05:16 +0800

    Time: 216 ms (62.55%), Space: 95.4 MB (53.53%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -4,33 +4,31 @@ public:
         return (i < 0) || (i > row-1) || (j < 0) || (j > col-1);
     }
 
-    void DFS(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int i, int j, vector<vector<bool>> &visited, bool &flag){
+    void DFS(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int i, int j, bool &flag){
         int row = grid2.size(), col = grid2[0].size();
 
-        if(outofbound(i, j, row, col) || visited[i][j] || grid2[i][j] == 0) return;
+        if(outofbound(i, j, row, col) || grid2[i][j] == 0) return;
         if(grid1[i][j] == 0){
             flag = false;
             return;
         }
-        visited[i][j] = true;
+        grid2[i][j] = 0;
 
-        DFS(grid1, grid2, i+1, j, visited, flag);
-        DFS(grid1, grid2, i-1, j, visited, flag);
-        DFS(grid1, grid2, i, j+1, visited, flag);
-        DFS(grid1, grid2, i, j-1, visited, flag);
+        DFS(grid1, grid2, i, j-1, flag);
+        DFS(grid1, grid2, i, j+1, flag);
+        DFS(grid1, grid2, i-1, j, flag);
+        DFS(grid1, grid2, i+1, j, flag);
     }
 
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
         int row = grid2.size(), col = grid2[0].size();
         int sub_island = 0;
 
-        vector<vector<bool>> visited(row, vector(col, false));
-
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){
-                if(grid2[i][j] && !visited[i][j]){
+                if(grid2[i][j]){
                     bool flag = true;
-                    DFS(grid1, grid2, i, j, visited, flag);
+                    DFS(grid1, grid2, i, j, flag);
                     if(flag) sub_island++;
                 }
             }

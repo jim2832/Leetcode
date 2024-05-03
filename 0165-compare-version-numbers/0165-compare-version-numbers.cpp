a0b8e594641491583b3f63154bb0ae0ce64725a3
Jim Lee <78035176+jim2832@users.noreply.github.com>
2024-05-03 17:00:52 +0800

    Time: 3 ms (39.03%), Space: 7.4 MB (52.42%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -3,11 +3,9 @@ public:
     int compareVersion(string version1, string version2) {
         int i = 0, j = 0;
         int n = version1.size(), m = version2.size();
-        int num1, num2;
 
         while(i < n || j < m){
-            num1 = 0;
-            num2 = 0;
+            int num1 = 0, num2 = 0;
 
             while(i < n && version1[i] != '.'){
                 num1 = num1 * 10 + (version1[i] - '0');

5c8c9fa407ae1adb58326b45ba77d90875d11141
Jim Lee <78035176+jim2832@users.noreply.github.com>
2024-08-26 16:55:18 +0800

    Time: 13 ms (64.15%), Space: 15.3 MB (21.73%) - LeetHub

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.cpp
@@ -0,0 +1,39 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> path;
+
+    void DFS(Node* root) {
+        if(!root) return;
+
+        for(auto &child : root->children){
+            DFS(child);
+        }
+
+        path.push_back(root->val);
+    }
+
+    vector<int> postorder(Node* root) {
+        DFS(root);
+        return path;
+    }
+};

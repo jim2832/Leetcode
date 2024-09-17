9189e495df7c577529fdcd301799a075cbc37172
Jim Lee <78035176+jim2832@users.noreply.github.com>
2024-09-17 18:01:53 +0800

    Time: 0 ms (100.00%), Space: 8.8 MB (72.35%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
@@ -3,9 +3,9 @@ public:
     vector<string> uncommonFromSentences(string s1, string s2) {
         string str = s1 + " " + s2;
         unordered_map<string, int> hash;
-
         int n = str.size();
         string temp;
+        
         for(int i=0; i<=n; i++){
             if(str[i] == ' ' || i == n){
                 hash[temp]++;

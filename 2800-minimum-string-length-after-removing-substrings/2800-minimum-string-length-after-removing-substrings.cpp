ec270b06fc9b653ef00704bc4014e38d6caac1cd
Jim Lee <78035176+jim2832@users.noreply.github.com>
2024-10-08 01:12:13 +0800

    Time: 3 ms (89.38%), Space: 11.6 MB (61.24%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -2,31 +2,16 @@ class Solution {
 public:
     int minLength(string s) {
         stack<char> stack;
-        char temp;
 
         for(auto &c : s){
-            stack.push(c);
-            if(c == 'B' && stack.size() > 1){
-                temp = c;
+            if(!stack.empty() && 
+                ((c == 'B' && stack.top() == 'A') || 
+                 (c == 'D' && stack.top() == 'C'))) {
                 stack.pop();
-                if(stack.top() == 'A'){
-                    stack.pop();
-                }
-                else{
-                    stack.push(temp);
-                }
             }
-            if(c == 'D' && stack.size() > 1){
-                temp = c;
-                stack.pop();
-                if(stack.top() == 'C'){
-                    stack.pop();
-                }
-                else{
-                    stack.push(temp);
-                }
+            else{
+                stack.push(c);
             }
-            // cout << stack.top();
         }
 
         return stack.size();

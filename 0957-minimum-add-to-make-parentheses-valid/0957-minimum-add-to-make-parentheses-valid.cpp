2496010d0ed04ff1faa16aafc84c83335ab54ef5
Jim Lee <78035176+jim2832@users.noreply.github.com>
2024-10-09 17:59:57 +0800

    Time: 0 ms (100%), Space: 7.7 MB (81.46%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int minAddToMakeValid(string s) {
@@ -13,5 +14,23 @@ public:
         }
 
         return stack.size() + count;
+    }
+};
+*/
+
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int left = 0, right = 0;
+
+        for(auto &c : s){
+            if(c == '(') left++;
+            else{
+                if(left > 0) left--;
+                else right++;
+            }
+        }
+
+        return left + right;
     }
 };

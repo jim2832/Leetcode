026d5ab2937d6b8d800cfd6ab4e6a73782ab39ba
Jim Lee <78035176+jim2832@users.noreply.github.com>
2024-11-04 23:06:56 +0800

    Time: 24 ms (15.6%), Space: 59.6 MB (11.13%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int i = num1.size()-1, j = num2.size()-1;
+        int carry = 0;
+        string result;
+
+        while(i >= 0 || j >= 0 || carry){
+            int sum = 0;
+            if(i >= 0) sum += num1[i--] - '0';
+            if(j >= 0) sum += num2[j--] - '0';
+            sum += carry;
+            carry = sum / 10;
+            result = to_string(sum % 10) + result;
+        }
+
+        return result;
+    }
+};

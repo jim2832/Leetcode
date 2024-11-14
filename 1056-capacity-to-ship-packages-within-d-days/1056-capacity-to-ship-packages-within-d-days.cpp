75a99d018686b4e05847edbd3bdfc1b4a93e6e4e
Jim Lee <78035176+jim2832@users.noreply.github.com>
2024-11-14 17:11:04 +0800

    Time: 11 ms (71.46%), Space: 34 MB (12.49%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
     int shipWithinDays(vector<int>& weights, int days) {
-        int n = weights.size();
-        
         int lower = *max_element(weights.begin(), weights.end()), upper = 25 * 1e6;
         while(lower <= upper){
             int mid = lower + (upper - lower) / 2; // 現在找到的值
-
-            int count = 0;
-            int i = 0;
-
-            while(i < n){
+            int count = 0, i = 0;
+            while(i < weights.size()){
                 int cur_sum = 0;
-                while(i < n && cur_sum + weights[i] <= mid){
-                    cur_sum += weights[i++];
-                }
+                while(i < weights.size() && cur_sum + weights[i] <= mid) cur_sum += weights[i++];
                 count++;
             }
-
-            if(count > days){
-                lower = mid + 1;
-            }
-            else{
-                upper = mid - 1;
-            }
+            count > days ? lower = mid + 1 : upper = mid - 1;
         }
 
         return lower;

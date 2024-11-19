1977583e30df55f9a360182ed628589aa1259e5a
Jim Lee <78035176+jim2832@users.noreply.github.com>
2024-11-19 18:04:12 +0800

    Time: 99 ms (87.63%), Space: 95.2 MB (77.52%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,27 +1,31 @@
-class Solution{
+class Solution {
 public:
-    long long maximumSubarraySum(vector<int>& nums, int k){
+    long long maximumSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> hash;
         int n = nums.size();
         if(k > n) return 0;
 
         long long sum = 0, maxi = 0;
-        unordered_map<int, int> hash;
-
         for(int i=0; i<k; i++){
-            hash[nums[i]]++;
             sum += nums[i];
+            hash[nums[i]]++;
         }
+
         if(hash.size() == k){
             maxi = sum;
         }
 
         for(int i=0; i<n-k; i++){
+            // remove the front element
             sum -= nums[i];
-            if(--hash[nums[i]] == 0) hash.erase(nums[i]);
+            hash[nums[i]]--;
+            if(hash[nums[i]] == 0) hash.erase(nums[i]);
 
-            sum += nums[i + k];
-            hash[nums[i + k]]++;
+            // add the back element
+            sum += nums[i+k];
+            hash[nums[i+k]]++;
 
+            // no element is duplicate
             if(hash.size() == k){
                 maxi = max(maxi, sum);
             }

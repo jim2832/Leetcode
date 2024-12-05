e348c54e6f8a2d7238c9a400b3f3988c3ad55846
Jim Lee <78035176+jim2832@users.noreply.github.com>
2024-12-05 23:45:01 +0800

    Time: 6 ms (81.61%), Space: 21.9 MB (33.33%) - LeetHub

---
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canChange(string start, string target) {
+        int n = start.size();
+        int i = 0, j = 0;
+
+        while(i < n || j < n){
+            while(i < n && start[i] == '_') i++;
+            while(j < n && target[j] == '_') j++;
+            if(start[i] != target[j]) return false;
+            if(start[i] == 'L' && i < j) return false;
+            if(start[i] == 'R' && i > j) return false;
+            i++;
+            j++;
+        }
+
+        return true;
+    }
+};

390265e6667d5b6fcae042c2d8fc2cc33e2edc8d
Jim Lee <78035176+jim2832@users.noreply.github.com>
2024-12-25 14:50:10 +0800

    Time: 0 ms (100%), Space: 22.8 MB (26.8%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
@@ -8,16 +8,16 @@ public:
         q.push(root);
 
         while(!q.empty()){
+            int maxi = INT_MIN;
             int n = q.size();
-            int current_max = INT_MIN;
             while(n--){
                 auto current = q.front();
                 q.pop();
                 if(current->left) q.push(current->left);
                 if(current->right) q.push(current->right);
-                current_max = max(current_max, current->val);
+                maxi = max(maxi, current->val);
             }
-            result.push_back(current_max);
+            result.push_back(maxi);
         }
 
         return result;

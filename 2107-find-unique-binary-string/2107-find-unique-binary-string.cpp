3dcb50535f236f7d56e88be5b1c7213a3b00f59c
Jim Lee <78035176+jim2832@users.noreply.github.com>
2025-02-21 01:54:49 +0800

    Time: 0 ms (100%), Space: 13 MB (47.18%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -2,15 +2,15 @@ class Solution{
 public:
     string findDifferentBinaryString(vector<string>& nums){
         unordered_set<string> s(nums.begin(), nums.end());
-        int n = nums[0].size(), num = 0;
-        string ans;
+        int n = nums.size(), num = 0;
+        string str;
 
         while(true){
-            ans = bitset<16>(num).to_string().substr(16 - n); // 確保是 n 位元
-            if(!s.count(ans)) break;
+            str = bitset<16>(num).to_string().substr(16 - n); // 確保是 n 位元
+            if(!s.count(str)) break;
             num++;
         }
 
-        return ans;
+        return str;
     }
 };

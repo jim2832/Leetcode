a1ed51342d171a109946aef289d389a396f92e4f
Jim Lee <78035176+jim2832@users.noreply.github.com>
2025-04-01 11:57:58 +0800

    Time: 27 ms (9.9%), Space: 211.5 MB (5%) - LeetHub

---
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long mostPoints(vector<vector<int>>& questions) {
+        int n = questions.size();
+        vector<long long> dp(200001, 0);
+
+        for(int i=n-1; i>=0; i--){
+            int point = questions[i][0];
+            int brainpower = questions[i][1];
+            dp[i] = max(dp[i+1], dp[i + brainpower + 1] + point);
+        }
+
+        return dp[0];
+    }
+};

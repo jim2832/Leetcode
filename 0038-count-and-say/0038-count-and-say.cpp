7b9b2fb57301ff764331daf14b5dca7e8a917a7b
Jim Lee <78035176+jim2832@users.noreply.github.com>
2025-04-18 15:36:02 +0800

    Time: 3 ms (83.88%), Space: 9.8 MB (72.29%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        //initial state
-        if(n == 1){
-            return "1";
-        }
-        if(n == 2){
-            return "11";
-        }
+        if(n == 1) return "1";
+        if(n == 2) return "11";
 
-        string str = "11"; //初始化
-        for(int i=3; i<=n; i++){
-            string temp = ""; //the string used for storing
-            str += "@"; //為了要比較
-            int count = 1; //計數
+        string str = "11";
+        for(int i = 3; i <= n; i++){
+            string temp;
+            int count = 1;
 
-            //走訪先前字串的每個字元
-            for(int j=0; j<str.length()-1; j++){
-                // 要處理不同數字的計數
+            for(int j = 0; j < str.size(); j++){
                 if(str[j] != str[j+1]){
-                    temp  = temp + to_string(count); //第一個數字(代表了計數)
-                    temp += str[j]; //第二個數字(代表了數字本身)
-                    count = 1; //reset
+                    temp += to_string(count) + str[j];
+                    count = 1;
                 }
-                else{
-                    count++;
-                }
+                else count++;
             }
+            
             str = temp;
         }
+
         return str;
     }
 };

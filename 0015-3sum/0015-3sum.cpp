db7546fd7a34f8296a0ffb9fdfe4699265440108
Jim Lee <78035176+jim2832@users.noreply.github.com>
2025-04-23 16:25:35 +0800

    Time: 47 ms (72.55%), Space: 28.9 MB (89.31%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,42 +1,28 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <set>
-using namespace std;
-
 class Solution {
-    public:
-        vector<vector<int>> threeSum(vector<int>& nums) {
-            sort(nums.begin(), nums.end()); //將input排序
-            int target = 0;
-            set<vector<int>> s; //存三個數的集合
-            vector<vector<int>> result;
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result;
 
-            for(int i=0; i<nums.size(); i++){
-                int j = i + 1; //第一個指標：從i+1開始
-                int k = nums.size() - 1; //第二個指標：從最後一個index開始
-                while(j < k){
-                    int sum = nums[i] + nums[j] + nums[k];
-                    //如果相加剛好是0
-                    if(sum == target){
-                        s.insert({nums[i], nums[j], nums[k]});
-                        j++;
-                        k--;
-                    }
-                    //如果相加<0
-                    else if(sum < target){
-                        j++;
-                    }
-                    //相加>0
-                    else{
-                        k--;
-                    }
+        for(int i = 0; i < n - 2; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
+            int j = i + 1, k = n - 1;
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum == 0){
+                    result.push_back({nums[i], nums[j], nums[k]});
+                    while(j < k && nums[j] == nums[j+1]) j++;
+                    while(j < k && nums[k] == nums[k-1]) k--;
+                    j++;
+                    k--;
                 }
+                else if(sum < 0) j++;
+                else k--;
             }
-
-            for(auto triplets:s){
-                result.push_back(triplets);
-            }
-            return result;
         }
+
+        return result;
+    }
 };

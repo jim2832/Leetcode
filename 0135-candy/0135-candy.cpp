937a4c435db4f26b4c3f10ceeec8779d68521535
Jim Lee <78035176+jim2832@users.noreply.github.com>
2025-06-02 13:31:09 +0800

    Time: 0 ms (100%), Space: 23 MB (27.4%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,41 +1,21 @@
-// 1. 每個小孩至少要1顆糖果
-// 2. 如果這個小孩的評分比鄰居高，則他要拿到比鄰居更多的糖果
-
-/*
-
-[1,0,2]
- 1 1 2 -> 2 1 2
-
-[1,2,2]
- 1 2 1
-
-*/
-
 class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
         vector<int> candies(n, 1);
 
-        // 從頭檢查一遍
-        for(int i=0; i<n-1; i++){
+        for(int i = 0; i < n - 1; i++){
             if(ratings[i+1] > ratings[i]){
                 candies[i+1] = candies[i] + 1;
             }
         }
 
-        //從尾檢查一遍是否有不合理的分配
-        for(int i=n-2; i>=0; i--){
+        for(int i = n - 2; i >= 0; i--){
             if(ratings[i] > ratings[i+1]){
                 candies[i] = max(candies[i], candies[i+1] + 1);
             }
         }
 
-        int result = 0;
-        for(auto ele:candies){
-            result += ele;
-        }
-
-        return result;
+        return accumulate(candies.begin(), candies.end(), 0);
     }
 };

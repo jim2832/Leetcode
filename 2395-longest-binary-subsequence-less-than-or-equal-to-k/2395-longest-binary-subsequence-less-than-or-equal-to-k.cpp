a177392b3a8426758c278ec205771dd748b23bac
Jim Lee <78035176+jim2832@users.noreply.github.com>
2025-06-27 01:57:04 +0800

    Time: 0 ms (100%), Space: 9.5 MB (73.55%) - LeetHub

---
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -0,0 +1,28 @@
+class Solution{
+public:
+    int longestSubsequence(string s, int k){
+        int count = 0, n = s.size();
+        long long cur_val = 0, weight = 1;
+
+        for(int i = n - 1; i >= 0; i--){
+            if(s[i] == '0'){
+                count++;
+            }
+            else{
+                if(weight <= k && cur_val + weight <= k){
+                    cur_val += weight;
+                    count++;
+                }
+            }
+
+            if(weight <= LLONG_MAX / 2){
+                weight *= 2;
+            }
+            else{
+                weight = LLONG_MAX;
+            }
+        }
+
+        return count;
+    }
+};

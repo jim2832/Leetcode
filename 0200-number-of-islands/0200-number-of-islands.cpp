277c2c2ede2b5c54af5e271ee0cb423c44edb425
Jim Lee <78035176+jim2832@users.noreply.github.com>
2025-07-07 03:47:06 +0800

    Time: 22 ms (88.9%), Space: 16.2 MB (89.53%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,63 +1,30 @@
-//此題為BFS之應用
-
 class Solution {
 public:
+    void DFS(int i, int j, vector<vector<char>>& grid){
+        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size()) return;
+        if(grid[i][j] == '0') return;
+
+        grid[i][j] = '0';
+
+        DFS(i + 1, j, grid);
+        DFS(i, j + 1, grid);
+        DFS(i - 1, j, grid);
+        DFS(i, j - 1, grid);
+    }
+
     int numIslands(vector<vector<char>>& grid) {
-        int rows = grid.size();
-        int cols = grid[0].size();
-        queue<int> Pixel; //用一個pixel來表示(x,y)座標
-        int island = 0;
+        int count = 0;
+        int row = grid.size(), col = grid[0].size();
 
-        // 0: 海洋
-        // 1: 未造訪過的路地
-        // 2: 已造訪過的路地
-        for(int i=0; i<rows; i++){
-            for(int j=0; j<cols; j++){
-                if(grid[i][j] == '1'){ //如果有發現陸地，則對他做BFS
-                    island++; //找到一個島嶼
-                    grid[i][j] = '2'; //代表已經走訪過
-                    int P = i * cols + j; //先算出pixel值
-                    Pixel.push(P);
-
-                    //BFS
-                    while(!Pixel.empty()){
-                        int current = Pixel.front();
-                        Pixel.pop();
-                        int x = current / cols;
-                        int y = current % cols;
-                        
-                        //向下走訪
-                        if(x+1 < rows && grid[x+1][y] == '1'){
-                            grid[x+1][y] = '2';
-                            int P_now = (x+1) * cols + y; //先算出pixel值
-                            Pixel.push(P_now);
-                        }
-
-                        //向上走訪
-                        if(x-1 >= 0 && grid[x-1][y] == '1'){
-                            grid[x-1][y] = '2';
-                            int P_now = (x-1) * cols + y; //先算出pixel值
-                            Pixel.push(P_now);
-                        }
-
-                        //向右走訪
-                        if(y+1 < cols && grid[x][y+1] == '1'){
-                            grid[x][y+1] = '2';
-                            int P_now = x * cols + (y+1); //先算出pixel值
-                            Pixel.push(P_now);
-                        }
-
-                        //向左走訪
-                        if(y-1 >= 0 && grid[x][y-1] == '1'){
-                            grid[x][y-1] = '2';
-                            int P_now = x * cols + (y-1); //先算出pixel值
-                            Pixel.push(P_now);
-                        }
-                    }
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(grid[i][j] == '1'){
+                    DFS(i, j, grid);
+                    count++;
                 }
             }
         }
 
-        return island;
+        return count;
     }
 };

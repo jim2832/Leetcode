c81243c0d5ec3a37bdc4a64ffa72d3d458ec85c9
Jim Lee <78035176+jim2832@users.noreply.github.com>
2025-07-15 18:56:34 +0800

    Time: 0 ms (100%), Space: 9.9 MB (60.64%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    void solver(vector<int>& nums, int index, vector<int>& sub, vector<vector<int>>& subs){
-        subs.push_back(sub);
-        for(int i=index; i<nums.size(); i++){
-            sub.push_back(nums[i]);
-            solver(nums, i+1, sub, subs);
-            sub.pop_back();
+    void solver(vector<int>& nums, int index, vector<int>& subset, vector<vector<int>> &result){
+        result.push_back(subset);
+        for(int i = index; i < nums.size(); i++){
+            subset.push_back(nums[i]);
+            solver(nums, i + 1, subset, result);
+            subset.pop_back();
         }
     }
 
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> subs;
-        vector<int> sub;
-        solver(nums, 0, sub, subs);
-
-        return subs;
+        vector<vector<int>> result;
+        vector<int> subset;
+        solver(nums, 0, subset, result);
+        return result;
     }
 };

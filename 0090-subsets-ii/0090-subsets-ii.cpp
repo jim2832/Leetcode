b976574c160600b7b07c4808451af85e208dc86a
Jim Lee <78035176+jim2832@users.noreply.github.com>
2025-07-15 19:41:09 +0800

    Time: 0 ms (100%), Space: 11 MB (29.45%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    void solver(vector<int>& nums, int index, vector<int>& sub, vector<vector<int>> &subs){
-        if(count(subs.begin(), subs.end(), sub) <= 0){
-            subs.push_back(sub);
+    void solver(vector<int>& nums, int index, vector<int>& subset, vector<vector<int>> &result, set<vector<int>> &us){
+        if(!us.count(subset)){
+            result.push_back(subset);
+            us.insert(subset);
         }
-        for(int i=index; i<nums.size(); i++){
-            sub.push_back(nums[i]);
-            solver(nums, i+1, sub, subs);
-            sub.pop_back();
+        for(int i = index; i < nums.size(); i++){
+            subset.push_back(nums[i]);
+            solver(nums, i + 1, subset, result, us);
+            subset.pop_back();
         }
     }
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        vector<vector<int>> subs;
-        vector<int> sub;
-        solver(nums, 0, sub, subs);
-
-        return subs;
+        set<vector<int>> us;
+        vector<vector<int>> result;
+        vector<int> subset;
+        solver(nums, 0, subset, result, us);
+        return result;
     }
 };

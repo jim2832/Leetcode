5e0f9d1fa5e1317cda0097ade05969b420abae14
Jim Lee <78035176+jim2832@users.noreply.github.com>
2025-07-16 02:21:17 +0800

    Time: 187 ms (92.14%), Space: 10.4 MB (86.94%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,40 +1,34 @@
-class Solution{
+class Solution {
 public:
-    bool DFS(int i, int j, vector<vector<char>>& board, const string& word, int cur_idx, vector<vector<bool>>& visited){
-        int row = board.size();
-        int col = board[0].size();
+    bool DFS(int i, int j, vector<vector<char>>& board, int index, const string &word){
+        if(i < 0 || i >= board.size() || j < 0 || j >= board[0].size()) return false;
+        if(board[i][j] != word[index]) return false;
 
-        // 越界 or 拜訪過 or 字母不符
-        if(i < 0 || j < 0 || i >= row || j >= col) return false;
-        if(visited[i][j]) return false;
-        if(board[i][j] != word[cur_idx]) return false;
+        if(index == word.size() - 1) return true; // matched
 
-        // 所有字母都匹配成功
-        if(cur_idx == word.size() - 1) return true;
+        char temp = board[i][j];
+        board[i][j] = '#';
 
-        visited[i][j] = true; // 標記拜訪
+        bool found = DFS(i, j+1, board, index+1, word) ||
+                    DFS(i+1, j, board, index+1, word) ||
+                    DFS(i, j-1, board, index+1, word) ||
+                    DFS(i-1, j, board, index+1, word);
+        
+        // backtracking
+        board[i][j] = temp;
 
-        // 四個方向探索
-        bool found = DFS(i+1, j, board, word, cur_idx+1, visited) ||
-                     DFS(i-1, j, board, word, cur_idx+1, visited) ||
-                     DFS(i, j+1, board, word, cur_idx+1, visited) ||
-                     DFS(i, j-1, board, word, cur_idx+1, visited);
-
-        visited[i][j] = false; // 回溯：解除標記
         return found;
     }
 
-    bool exist(vector<vector<char>>& board, string word){
-        int row = board.size();
-        int col = board[0].size();
-
-        vector<vector<bool>> visited(row, vector<bool>(col, false));
+    bool exist(vector<vector<char>>& board, string word) {
+        int row = board.size(), col = board[0].size();
 
         for(int i = 0; i < row; i++){
             for(int j = 0; j < col; j++){
                 if(board[i][j] == word[0]){
-                    if(DFS(i, j, board, word, 0, visited))
+                    if(DFS(i, j, board, 0, word)){
                         return true;
+                    }
                 }
             }
         }

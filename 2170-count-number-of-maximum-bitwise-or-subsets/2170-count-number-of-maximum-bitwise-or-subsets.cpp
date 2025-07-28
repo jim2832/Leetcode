eac8737753f202123586cf62950bbcd87ae5de9d
Jim Lee <78035176+jim2832@users.noreply.github.com>
2025-07-28 23:11:48 +0800

    Time: 8 ms (80.56%), Space: 10.6 MB (86.47%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    void backtracking(vector<int> &nums, int current, int target, int idx, int &count){
-        if(idx == nums.size()){
+    void solve(vector<int>& nums, int index, int current, int target, int &count){
+        if(index == nums.size()){
             if(current == target){
                 count++;
             }
             return;
         }
-        backtracking(nums, current | nums[idx], target, idx+1, count);
-        backtracking(nums, current, target, idx+1, count);
+        solve(nums, index + 1, current | nums[index], target, count);
+        solve(nums, index + 1, current, target, count);
     }
 
     int countMaxOrSubsets(vector<int>& nums) {
         int target = 0, count = 0;
 
-        for(auto &num : nums){
+        for(int num : nums){
             target |= num;
         }
 
-        backtracking(nums, 0, target, 0, count);
-
+        solve(nums, 0, 0, target, count);
+        
         return count;
     }
 };

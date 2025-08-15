08a901a68280246fb5ab89bbbd38e4f86f67ff97
Jim Lee <78035176+jim2832@users.noreply.github.com>
2025-08-15 14:52:37 +0800

    Time: 0 ms (100%), Space: 7.9 MB (18.14%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n == 0){
-            return false;
-        }
+        if(n == 0) return false;
 
-        while(n){
-            if(n % 4 != 0 && n != 1){
-                return false;
-            }
+        while(n != 1){
+            if(n % 4) return false;
             n /= 4;
         }
 

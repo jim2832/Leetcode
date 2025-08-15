cdb52d88563f117de8e92d9ccede46f06c937d2f
Jim Lee <78035176+jim2832@users.noreply.github.com>
2025-08-15 16:03:43 +0800

    Time: 0 ms (100%), Space: 23.1 MB (56.56%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -1,25 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-    public:
-        bool result;
+public:
+    int solve(TreeNode* root){
+        if(!root) return 0;
 
-        int check_balance(TreeNode* root){
-            if(root == nullptr){
-                return 0;
-            }
-            if(!result){
-                return 0;
-            }
-            int left_sub_tree = check_balance(root->left); //往左子樹走訪
-            int right_sub_tree = check_balance(root->right); //往右子樹走訪
-            if(abs(left_sub_tree - right_sub_tree) > 1){
-                result = false;
-            }
-            return 1 + max(left_sub_tree, right_sub_tree);
+        int left_height = solve(root->left);
+        if(left_height == -1){
+            return -1;
         }
 
-        bool isBalanced(TreeNode* root) {
-            result = true;
-            int temp = check_balance(root);
-            return result;
+        int right_height = solve(root->right);
+        if(right_height == -1){
+            return -1;
         }
+
+        if(abs(left_height - right_height) > 1){
+            return -1;
+        }
+
+        return 1 + max(left_height, right_height);
+    }
+
+    bool isBalanced(TreeNode* root) {
+        return solve(root) != -1;
+    }
 };

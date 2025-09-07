16cedb2417464e6d526b6b0b87159d0dda2864be
Jim Lee <78035176+jim2832@users.noreply.github.com>
2025-09-07 23:51:16 +0800

    Time: 0 ms (100%), Space: 9.7 MB (53.26%) - LeetHub

--- 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.cpp
+++ 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     vector<int> sumZero(int n) {
-        int max = n / 2;
         vector<int> result;
 
-        for(int num = -max; num < 0; num++){
+        for(int num = 1; num <= n / 2; num++){
             result.push_back(num);
+            result.push_back(-num);
         }
 
         if(n % 2) result.push_back(0);
-
-        for(int num = 1; num <= max; num++){
-            result.push_back(num);
-        }
 
         return result;
     }

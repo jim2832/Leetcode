e714b8089a7ae273bb5734d3c91ed3815fe5ca4a
Jim Lee <78035176+jim2832@users.noreply.github.com>
2025-09-16 01:15:18 +0800

    Time: 0 ms (100%), Space: 9 MB (57.81%) - LeetHub

---
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters) {
+        unordered_set<char> us;
+        int n = text.size();
+        int count = 0;
+        bool flag = true;
+
+        for(char c : brokenLetters){
+            us.insert(c);
+        }
+
+        for(int i = 0; i <= n; i++){
+            if(text[i] == ' ' || i == n){
+                if(flag == true){
+                    count++;
+                }
+                flag = true;
+            }
+            else{
+                if(us.count(text[i])){
+                    flag = false;
+                }
+            }
+        }
+
+        return count;
+    }
+};

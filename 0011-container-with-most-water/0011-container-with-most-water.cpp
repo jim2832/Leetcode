4145d2b54ad7c84713b037131b822b06aeb2d2a6
Jim Lee <78035176+jim2832@users.noreply.github.com>
2025-10-05 01:35:22 +0800

    Time: 0 ms (100%), Space: 63 MB (12.56%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,41 +1,14 @@
 class Solution {
 public:
-
-    // brutal force
-    /*
     int maxArea(vector<int>& height) {
-        int max = 0, flow = 0;
-        int h, w;
-
-        for(int i=0; i<height.size()-1; i++){
-            for(int j=i+1; j<height.size(); j++){
-                h = min(height[i], height[j]);
-                w = abs(i-j);
-                flow = h * w;
-                max = flow > max ? flow : max;
-            }
-        }
-        return max;
-    }
-    */
-
-    // another approach
-    int maxArea(vector<int>& height) {
-        int i = 0, j = height.size()-1;
-        int max = 0, flow = 0;
-        int h, w;
+        int i = 0, j = height.size() - 1;
+        int maxi = INT_MIN;
 
         while(i < j){
-            h = min(height[i], height[j]);
-            w = abs(i-j);
-            flow = h * w;
-            max = flow > max ? flow : max;
-            
-            // 如果水管沒有更高，就不可能有更多的水量，因此跳過比現在矮的水管
-            while(height[i] <= h && i < j) i++;
-            while(height[j] <= h && i < j) j--;
+            maxi = max(maxi, (j - i) * min(height[i], height[j]));
+            height[i] < height[j] ? i++ : j--;
         }
 
-        return max;
+        return maxi;
     }
 };
